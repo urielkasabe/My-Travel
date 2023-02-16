17328ce945af6ce491ebdba09072293a4b1cd6a0
Uriel Kasabe <urielkasabe5@gmail.com>
2023-02-16 10:50:02 +0200

    added DestinationsPage.vue and added as a route on main.js

--- src/main.js
+++ src/main.js
@@ -6,18 +6,21 @@ import App from './App.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
 import TheFooter from './components/layouts/TheFooter.vue';
 import BaseContent from './components/UI/BaseContent.vue';
+import DestinationsPage from './components/UI/DestinationsPage.vue';
 
 const app = createApp(App)
 
 app.component('the-header', TheHeader);
 app.component('the-footer', TheFooter);
 app.component('base-content', BaseContent );
+app.component('destinations-page', DestinationsPage);
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [ 
         { path: '/', redirect: '/homepage' },
         { path: '/homepage', component:  BaseContent },
+        { path: '/destinations', component: DestinationsPage },
     ],
     linkActiveClass: 'active',
 });
