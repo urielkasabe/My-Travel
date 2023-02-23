f970800d9e41f6a1b8827515f3eaa1dac50a2891
Uriel Kasabe <urielkasabe5@gmail.com>
2023-02-23 18:22:43 +0200

    new notfound route

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ const router = createRouter({
         { path: '/homepage', component:  BaseContent },
         { path: '/destinations', component: DestinationsPage },
         { path: '/menu', component: TheMenu },
-        { path: '/:notfound(.*)', redirect: '/homepage' }
+        { path: '/:pathMatch(.*)*', redirect: '/' }
     ],
     linkActiveClass: 'active',
 });
